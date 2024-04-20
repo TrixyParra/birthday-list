719ac080b60decd578f69c9c0f5f5aa5c76f113d
Trixy Parra <trixy.parra@gmail.com>
2024-04-20 00:42:31 -0700

    view all button and close button event listeners

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-const viewAll = document.querySelector('#view-all')
+const viewAllBtn = document.querySelector('#view-all-btn')
+const closeBtn = document.querySelector('#close-btn')
 const birthdayList = document.querySelectorAll('.birthday-list')
 
-viewAll.addEventListener('click', () => {
+// View All Button 
+viewAllBtn.addEventListener('click', () => {
+    birthdayList.forEach((birthday) => {
+        birthday.classList.toggle("hidden");
+    });
+
+    viewAllBtn.classList.add("hidden");
+
+    closeBtn.classList.remove("hidden");
+})
+
+// View Less Button 
+closeBtn.addEventListener('click', () => {
     birthdayList.forEach((birthday) => {
         birthday.classList.toggle("hidden");
     })
+
+    closeBtn.classList.add("hidden");
+
+    viewAllBtn.classList.remove("hidden");
 })
